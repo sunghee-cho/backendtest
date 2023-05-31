b085e8548198dcf3574f921cf30dba87fb4c4161
cho <bluejeansh@hanmail.net>
2023-05-31 09:42:43 +0900

    test2 브랜치 생성

--- git_boot/src/main/java/com/example/demo/HelloController.java
+++ git_boot/src/main/java/com/example/demo/HelloController.java
@@ -10,7 +10,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 @RequestMapping("/helloboot")
 public ModelAndView helloboot() {
-	System.out.println("test1 브랜치에 수정ㄴ");
+	System.out.println("test2 브랜치 수정");
+	
 	ModelAndView mv = new ModelAndView();
 	mv.addObject("model", "스프링부트를 시작합니다");
 	mv.setViewName("hello");
